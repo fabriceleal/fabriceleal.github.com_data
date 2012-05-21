733bbc7c79ff8bfedeac6bb85e15e84194cfa361
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-21 14:27:26 -0700

    repositories.sh changed

---
+++ detailed_repo_info.js
@@ -0,0 +1,16 @@
+function(name, setter){
+     var output = '';
+     fork(
+             'curl',
+             [ 'https://api.github.com/repos/fabriceleal/' + name ],
+             null,
+             function(data){
+                     output += data;
+             },
+             null,
+             function(code){
+                     var data = JSON.parse(output);
+                     setter(data);
+                     return true;
+             });
+}
