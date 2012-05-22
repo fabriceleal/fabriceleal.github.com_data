b2f8b3676481fe9f733657658c7b0ed1842ef7a7
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-22 05:38:07 -0700

    fixed languages, all ok now

--- languages.sh
+++ languages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../jed/jed.js -m "' _.name '" | ../jed/jed.js -ma "./languages_repos_langs.js" | ../jed/jed.js -rf "./languages_sum.js" -i "{}" | ../jed/jed.js -rf "languages_dict_to_arr.js" -i "[]" | ../jed/jed.js -df "./languages_sort.js"
+../jed/jed.js -m "' _.name '" | ../jed/jed.js -maf "./languages_repos_langs.js" | ../jed/jed.js -rf "./languages_sum.js" -i "{}" | ../jed/jed.js -rf "languages_dict_to_arr.js" -i "[]" | ../jed/jed.js -df "./languages_sort.js"
