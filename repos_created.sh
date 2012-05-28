b4a31d23008a615589ec21795db6b6c85da6834a
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-28 14:50:48 -0700

    forgot to add one file *facepalm*

--- repos_created.sh
+++ repos_created.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-../jed/jed.js -m "'[ _.name, _.created_at ]'" | ../jed/jed.js -df "./repos_created_agreg.js" | ./transform_to_dict.sh
+jsoned -m "'[ _.name, _.created_at ]'" | jsoned -df "./repos_created_agreg.js" | ./transform_to_dict.sh
 
 
