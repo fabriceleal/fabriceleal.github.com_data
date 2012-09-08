9b03be96c2e26a00fe5c1cb23d5decf4848232c3
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-09-08 18:41:14 +0100

    funky sorting ...

--- transform_to_dict_do.js
+++ transform_to_dict_do.js
@@ -3,6 +3,10 @@ function(arg){
 	for(var i in arg){
 		ret.push([i, arg[i]]);
 	} 
+
 	// ascendent
-	return ret.sort(function(a, b){ return a > b; }); 
+	return ret.sort(function(a, b){ 
+			//console.log(a[0], '>', b[0], '=', a[0] > b[0]); 
+			return a[0] > b[0]; 
+	});
 }
